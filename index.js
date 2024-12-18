7ac152600d62a55d20fd39b705ac82ba90f42122
Douglas Lima <douglas.Lima@AB-inbev.com>
2024-12-18 10:53:18 -0300

    [Douglas] hello world test

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-  res.render('pages/index')
+  res.json({ message: 'Hello World' })
 })
 
 const server = app.listen(port, () => {
