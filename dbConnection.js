fe478fd405f72d91582efafce8356cca36537817
Hugo Rafael <hugo.rafael.alves@gmail.com>
2024-12-18 16:55:04 -0300

    remove db string

--- dbConnection.js
+++ dbConnection.js
@@ -1,7 +1,7 @@
 const { Client } = require('pg');
 
 const client = new Client({
-  connectionString: 'postgres://u287lc3durc8fe:p51cd5af28a35dabe56de2198085fe56b037df90b5b390c7e98c269f70f5d534c@cfls9h51f4i86c.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dfe1v0oj3rbfqs',
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
